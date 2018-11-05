65502bdc51dd2cb7f9206525bebf14de7358bba8
Sihong Huang <sihongspring2011@gmail.com>
2018-11-04 19:28:19 -0500

    use sendgrid for emailing password reset, set up password reset, work on authorization protection

--- controllers/admin.js
+++ controllers/admin.js
@@ -70,16 +70,18 @@ exports.postEditProduct = (req, res, next) => {
               
     Product.findById(prodId)
         .then(product => {
+            if (product.userId.toString() !== req.user._id.toString()) {
+                return res.redirect('/');
+            }
             product.title = updatedTitle;
             product.price = updatedPrice;
             product.description = updatedDescription;
             product.imageUrl = updatedImageUrl
             // mongoose save will automatically check if product already exists and update the existing product instead of creating a new one
-            return product.save();
-        })
-        .then(result => {
-            console.log('UPDATED PRODUCT');
-            res.redirect('/admin/products');
+            return product.save().then(result => {
+                console.log('UPDATED PRODUCT');
+                res.redirect('/admin/products');
+            });
         })
         .catch(err => { 
             console.log(err);
@@ -88,7 +90,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByIdAndRemove(prodId)
+    Product.deleteOne({_id: prodId, userId: req.user._id})
         .then(() => {
             console.log('Product Deleted');
             res.redirect('/admin/products');
@@ -99,7 +101,7 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.find()
+    Product.find({ userId: req.user._id})
         // .select allows you to choose which fields to display or which fields to remove from display
         // .populate allows you to fetch the full set of data from a reference id, and also pick which fields to display as a second arg
         // .select('title price -_id')
